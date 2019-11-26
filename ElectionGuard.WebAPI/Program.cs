49e2295c8f092bb1ab0a530e3accc798b84859bd
Matt Wilhelm <github@addressxception.com>
2019-11-26 12:34:04 -0500

    ⚙️ Configure app to run as a windows service
    Add script for creating windows service

--- ElectionGuard.WebAPI/Program.cs
+++ ElectionGuard.WebAPI/Program.cs
@@ -11,13 +11,14 @@ namespace ElectionGuard.WebAPI
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            await CreateHostBuilder(args).Build().RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseWindowsService()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
