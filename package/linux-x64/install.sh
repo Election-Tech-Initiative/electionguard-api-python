c28437a09d889fd82885ce924738d45a28f82180
Matt <github@addressxception.com>
2019-11-30 14:36:50 -0500

    Configure app to run as a systemd service

---
+++ package/linux-x64/install.sh
@@ -0,0 +1,33 @@
+!#/usr/bin/bash
+
+SERVICE_NAME='electionguard-api.service'
+
+echo "stopping bin services"
+sudo systemctl stop $SERVICE_NAME
+sudo systemctl disable $SERVICE_NAME
+sudo rm -rf /etc/systemd/system/$SERVICE_NAME
+
+if [ ! -d "/bin/electionguard" ]; then
+        sudo mkdir /bin/electionguard
+fi
+    
+sudo rm -rf /bin/electionguard/*
+
+sudo tar -zxvf electionguard-api.tar.gz -C /bin/electionguard/
+
+sudo chmod -R 777 /bin/electionguard
+
+sudo cp /bin/electionguard/$SERVICE_NAME /etc/systemd/system/$SERVICE_NAME
+
+echo "reloading services"
+sudo systemctl daemon-reload
+sudo systemctl reset-failed
+
+echo "enabling services"
+sudo systemctl enable $SERVICE_NAME
+
+echo "starting bin services"
+sudo systemctl start $SERVICE_NAME
+systemctl status $SERVICE_NAME
+
+echo "done"
