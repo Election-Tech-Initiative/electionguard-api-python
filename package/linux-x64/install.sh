1ddf8e58ead6e12d88389e41d147052149cd1112
Matt <github@addressxception.com>
2019-12-10 13:02:42 -0500

    guard dotnet installation if exists

--- package/linux-x64/install.sh
+++ package/linux-x64/install.sh
@@ -2,17 +2,19 @@
 
 SERVICE_NAME='electionguard-api.service'
 
-echo "installing dependencies"
+command -v dotnet >/dev/null 2>&1 || {
+        echo "installing dotnet"
 
-wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-sudo dpkg -i packages-microsoft-prod.deb
+        wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+        sudo dpkg -i packages-microsoft-prod.deb
 
-sudo add-apt-repository universe
-sudo apt-get update
-sudo apt-get install apt-transport-https
-sudo apt-get update
-sudo apt-get install dotnet-runtime-3.0
-sudo apt-get install aspnetcore-runtime-3.0
+        sudo add-apt-repository universe
+        sudo apt-get update
+        sudo apt-get install apt-transport-https
+        sudo apt-get update
+        sudo apt-get install dotnet-runtime-3.0
+        sudo apt-get install aspnetcore-runtime-3.0
+}
 
 echo "stopping ElectionGuard services"
 sudo systemctl stop $SERVICE_NAME
