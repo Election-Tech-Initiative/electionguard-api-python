2794713ca40363de52fa7299a27f0916de98d574
Matt <github@addressxception.com>
2019-12-10 11:12:49 -0500

    install dependencies in install script

--- package/linux-x64/install.sh
+++ package/linux-x64/install.sh
@@ -2,6 +2,18 @@
 
 SERVICE_NAME='electionguard-api.service'
 
+echo "installing dependencies"
+
+wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+
+sudo add-apt-repository universe
+sudo apt-get update
+sudo apt-get install apt-transport-https
+sudo apt-get update
+sudo apt-get install dotnet-runtime-3.0
+sudo apt-get install aspnetcore-runtime-3.0
+
 echo "stopping ElectionGuard services"
 sudo systemctl stop $SERVICE_NAME
 sudo systemctl disable $SERVICE_NAME
