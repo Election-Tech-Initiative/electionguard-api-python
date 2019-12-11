1a661fbc86f14df49c2fba4473051df97ff8ac26
Aaron Ingram <aaron.ingram@infernored.com>
2019-12-11 11:41:14 -0500

    Allow deserialization to derived Contest types
    Prior to this change, incoming Contest types were deserialized to the base Contest type, which prevented us from accessing the type-specific properties.
    
    This change adds a custom JSON converter that uses the type property in the JSON to determine the correct derived type during deserialization.
    # Provide links to any relevant tickets, articles or other resources

--- VotingWorks.Ballot/Contest.cs
+++ VotingWorks.Ballot/Contest.cs
@@ -1,5 +1,10 @@
-﻿namespace VotingWorks.Ballot
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace VotingWorks.Ballot
 {
+    [JsonConverter(typeof(ContestConverter))]
     public class Contest
     {
         public string Id { get; set; }
@@ -7,6 +12,21 @@
         public string? PartyId { get; set; }
         public string Section { get; set; }
         public string Title { get; set; }
-        public string Type { get; set; }
+        public ContestType Type { get; set; }
+
+        private class ContestConverter : CustomJsonConverter<Contest> {
+            protected override Contest Create(Type objectType, JObject jObject)
+            {
+                var contestType = Enum.Parse<ContestType>(jObject.Value<string>("type"), true);
+                switch(contestType) {
+                    case ContestType.Candidate:
+                        return new CandidateContest();
+                    case ContestType.YesNo:
+                        return new YesNoContest();
+                    default:
+                        return new Contest();
+                }
+            }
+        }
     }
 }
