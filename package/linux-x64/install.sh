4f8dbcba9bdc7b89766a2ccd9d7297d13147611a
Matt <github@addressxception.com>
2019-12-11 11:03:59 -0500

    auto accept installation of packages when they do not exist

--- package/linux-x64/install.sh
+++ package/linux-x64/install.sh
@@ -8,12 +8,12 @@ command -v dotnet >/dev/null 2>&1 || {
         wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
         sudo dpkg -i packages-microsoft-prod.deb
 
-        sudo add-apt-repository universe
+        sudo add-apt-repository -y universe
         sudo apt-get update
-        sudo apt-get install apt-transport-https
+        sudo apt-get install -y apt-transport-https
         sudo apt-get update
-        sudo apt-get install dotnet-runtime-3.0
-        sudo apt-get install aspnetcore-runtime-3.0
+        sudo apt-get install -y dotnet-runtime-3.0
+        sudo apt-get install -y aspnetcore-runtime-3.0
 }
 
 echo "stopping ElectionGuard services"
@@ -42,6 +42,6 @@ sudo systemctl enable $SERVICE_NAME
 
 echo "starting ElectionGuard services"
 sudo systemctl start $SERVICE_NAME
-systemctl status $SERVICE_NAME
+systemctl status $SERVICE_NAME --no-pager
 
 echo "done"
