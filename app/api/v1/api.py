498909cdf09352405c76d22706450f8a0e3e81ae
Keith Fung <keith.fung@infernored.com>
2020-09-01 12:28:04 -0400

    ✨ Implement Cast/Spoil Ballot and Default Election Description
    - Implement calls to cast/spoil ballots
    - Implement calls to get default election description
    - Add serialization methods until new electionguard package is pulled in
    - Add jsons package
    - Add electionguard package

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
-from app.api.v1.endpoints import ping
+from app.api.v1.endpoints import ballot, ping, election
 
 api_router = APIRouter()
 api_router.include_router(ping.router, prefix="/ping", tags=["ping"])
+api_router.include_router(election.router, prefix="/election", tags=["election"])
+api_router.include_router(ballot.router, prefix="/ballot", tags=["ballot"])
