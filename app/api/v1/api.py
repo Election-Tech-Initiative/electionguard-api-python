e1cc706cd125a0ec870374ed53e8a21bdb9cfd95
Ryan Korsak <rkorsak@gmail.com>
2020-09-04 15:53:50 -0400

    ✨ Accumulate tally endpoint (#62)
    * New endpoint for tally accumulation
    
    * Update the Postman collection with sample tally accumulation requests

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 
-from app.api.v1.endpoints import ballot, election, key, ping
+from app.api.v1.endpoints import ballot, election, key, ping, tally
 
 api_router = APIRouter()
 api_router.include_router(ping.router, prefix="/ping", tags=["ping"])
 api_router.include_router(election.router, prefix="/election", tags=["election"])
 api_router.include_router(ballot.router, prefix="/ballot", tags=["ballot"])
 api_router.include_router(key.router, prefix="/key", tags=["key"])
+api_router.include_router(tally.router, prefix="/tally", tags=["tally"])
