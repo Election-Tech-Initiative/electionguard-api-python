c4b73dbff22a65871a3aa5cde3c0b5ca7fceb9f8
Ryan Korsak <rkorsak@gmail.com>
2020-09-08 09:41:59 -0400

    🔀 Reorganized tally endpoints into tally and tally/append (#63)

--- app/api/v1/endpoints/tally.py
+++ app/api/v1/endpoints/tally.py
@@ -11,58 +11,75 @@ from electionguard.tally import (
 )
 from fastapi import APIRouter, Body, HTTPException
 from pydantic import BaseModel
-from typing import Any, List, Optional
+from typing import Any, List, Tuple
 
 router = APIRouter()
 
 
-class AccumulateTallyRequest(BaseModel):
+class StartTallyRequest(BaseModel):
     ballots: List[Any]
-    encrypted_tally: Optional[Any] = None
     description: Any
     context: Any
 
 
-@router.post("/accumulate")
-def accumulate_tally(request: AccumulateTallyRequest = Body(...)) -> Any:
+class AppendTallyRequest(StartTallyRequest):
+    encrypted_tally: Any
+
+
+@router.post("")
+def start_tally(request: StartTallyRequest = Body(...)) -> Any:
     """
-    Accumulate ballots into a new or existing tally
+    Start a new tally of a collection of ballots
     """
 
+    ballots, description, context = parse_request(request)
+    tally = CiphertextTally("election-results", description, context)
+
+    return tally_ballots(tally, ballots)
+
+
+@router.post("/append")
+def append_to_tally(request: AppendTallyRequest = Body(...)) -> Any:
+    """
+    Append ballots into an existing tally
+    """
+
+    ballots, description, context = parse_request(request)
+
+    published_tally = PublishedCiphertextTally.from_json_object(request.encrypted_tally)
+    tally = CiphertextTally(published_tally.object_id, description, context)
+    tally.cast = published_tally.cast
+
+    return tally_ballots(tally, ballots)
+
+
+def parse_request(
+    request: StartTallyRequest,
+) -> Tuple[
+    List[CiphertextAcceptedBallot],
+    InternalElectionDescription,
+    CiphertextElectionContext,
+]:
+    """
+    Deserialize common tally request values
+    """
     ballots = [
         CiphertextAcceptedBallot.from_json_object(ballot) for ballot in request.ballots
     ]
     description = ElectionDescription.from_json_object(request.description)
     internal_description = InternalElectionDescription(description)
     context = CiphertextElectionContext.from_json_object(request.context)
-    published_tally: Optional[PublishedCiphertextTally] = (
-        PublishedCiphertextTally.from_json_object(request.encrypted_tally)
-        if request.encrypted_tally
-        else None
-    )
 
-    tally = _get_new_or_existing_tally(internal_description, context, published_tally)
+    return (ballots, internal_description, context)
 
+
+def tally_ballots(
+    tally: CiphertextTally, ballots: List[CiphertextAcceptedBallot]
+) -> PublishedCiphertextTally:
     tally_succeeded = tally.batch_append(ballots)
 
     if tally_succeeded:
         published_tally = publish_ciphertext_tally(tally)
         return published_tally.to_json_object()
     else:
-        raise HTTPException(
-            status_code=500, detail="Tally accumulation was unsuccessful"
-        )
-
-
-def _get_new_or_existing_tally(
-    description: ElectionDescription,
-    context: CiphertextElectionContext,
-    published_tally: PublishedCiphertextTally,
-) -> CiphertextTally:
-    if not published_tally:
-        return CiphertextTally("election-results", description, context)
-
-    full_tally = CiphertextTally(published_tally.object_id, description, context)
-    full_tally.cast = published_tally.cast
-
-    return full_tally
+        raise HTTPException(status_code=500, detail="Unable to tally ballots")
