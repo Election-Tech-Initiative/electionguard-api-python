6b1b96c4f047ba80d9457e7f08cc8f0f60ddc729
Ryan Korsak <rkorsak@gmail.com>
2020-09-13 12:34:16 -0400

    🔓 Tally decryption endpoints (#73)
    * 🔓 Tally decryption endpoints
    
    Adds the necessary endpoints to compute guardian shares and decrypt a tally *when all guardians are present*.
    
    * Add guardian-only tag to share decryption endpoint
    
    * Added tally decryption to Postman
    
    * Added docstrings for tally decryption endpoints
    
    * Fix some bugs introduced in tally decryption

--- app/api/v1/models/tally.py
+++ app/api/v1/models/tally.py
@@ -1,13 +1,58 @@
-from typing import Any, List
+from typing import Any, Dict, List
+import electionguard.election
+import electionguard.tally
 
+from app.utils.serialize import read_json_object
+
+from .ballot import CiphertextAcceptedBallot
 from .base import Base
+from .election import ElectionDecription, CiphertextElectionContext
+from .guardian import Guardian
+
+
+PublishedCiphertextTally = Any
+TallyDecryptionShare = Any
 
 
 class StartTallyRequest(Base):
-    ballots: List[Any]
-    description: Any
-    context: Any
+    ballots: List[CiphertextAcceptedBallot]
+    description: ElectionDecription
+    context: CiphertextElectionContext
 
 
 class AppendTallyRequest(StartTallyRequest):
-    encrypted_tally: Any
+    encrypted_tally: PublishedCiphertextTally
+
+
+class DecryptTallyRequest(Base):
+    encrypted_tally: PublishedCiphertextTally
+    shares: Dict[str, TallyDecryptionShare]
+    description: ElectionDecription
+    context: CiphertextElectionContext
+
+
+class DecryptTallyShareRequest(Base):
+    encrypted_tally: PublishedCiphertextTally
+    guardian: Guardian
+    description: ElectionDecription
+    context: CiphertextElectionContext
+
+
+def convert_tally(
+    encrypted_tally: PublishedCiphertextTally,
+    description: electionguard.election.InternalElectionDescription,
+    context: electionguard.election.CiphertextElectionContext,
+) -> electionguard.tally.CiphertextTally:
+    """
+    Convert to an SDK CiphertextTally model
+    """
+
+    published_tally = read_json_object(
+        encrypted_tally, electionguard.tally.PublishedCiphertextTally
+    )
+    tally = electionguard.tally.CiphertextTally(
+        published_tally.object_id, description, context
+    )
+    tally.cast = published_tally.cast
+
+    return tally
