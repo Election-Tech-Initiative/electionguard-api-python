3035b28a7bb376dfdb11470ea67404a72eee33df
Ryan Korsak <rkorsak@gmail.com>
2020-09-15 16:32:07 -0400

    🔐 Add an endpoint to encrypt a batch of ballots

--- app/api/v1/models/ballot.py
+++ app/api/v1/models/ballot.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, List, Optional
 
 from .base import Base
 from .election import ElectionDecription, CiphertextElectionContext
@@ -6,9 +6,23 @@ from .election import ElectionDecription, CiphertextElectionContext
 
 CiphertextAcceptedBallot = Any
 CiphertextBallot = Any
+PlaintextBallot = Any
 
 
 class AcceptBallotRequest(Base):
     ballot: CiphertextBallot
     description: ElectionDecription
     context: CiphertextElectionContext
+
+
+class EncryptBallotsRequest(Base):
+    ballots: List[PlaintextBallot]
+    seed_hash: str
+    nonce: Optional[str] = None
+    description: ElectionDecription
+    context: CiphertextElectionContext
+
+
+class EncryptBallotsResponse(Base):
+    encrypted_ballots: List[PlaintextBallot]
+    tracking_hash: str
