23638fc688e12a91824fab09f38a0689815b01e2
Lee Richardson <github@leerichardson.com>
2022-01-05 15:54:27 -0500

    Fix failing integration tests

--- app/api/v2/routes.py
+++ app/api/v2/routes.py
@@ -3,6 +3,7 @@ from app.api.v1 import auth
 from app.core.settings import ApiMode, Settings
 from . import common
 from . import mediator
+from . import guardian
 
 
 def get_v2_routes(settings: Settings) -> APIRouter:
@@ -11,7 +12,7 @@ def get_v2_routes(settings: Settings) -> APIRouter:
     api_router.include_router(auth.router)
 
     if settings.API_MODE == ApiMode.GUARDIAN:
-        raise ValueError(f"Guardian endpoints are  currently only available in v1")
+        api_router.include_router(guardian.router)
     elif settings.API_MODE == ApiMode.MEDIATOR:
         api_router.include_router(mediator.router)
     else:
