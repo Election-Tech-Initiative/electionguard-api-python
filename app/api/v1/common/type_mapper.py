85b14b82b6e4a0901122a3ceb60149303ebb2d43
Lee Richardson <github@leerichardson.com>
2022-02-02 17:16:22 -0500

    Support hex int's

--- app/api/v1/common/type_mapper.py
+++ app/api/v1/common/type_mapper.py
@@ -1,17 +1,31 @@
-from electionguard.group import ElementModP, ElementModQ, hex_to_p, hex_to_q
+from typing import Union
+from electionguard.group import (
+    ElementModP,
+    ElementModQ,
+    hex_to_p,
+    hex_to_q,
+    int_to_p,
+    int_to_q,
+)
 
 
 class TypeMapper:
     @staticmethod
-    def string_to_elementModP(value: str) -> ElementModP:
-        elementModP = hex_to_p(value)
-        if elementModP is None:
+    def string_to_elementModP(value: Union[int, str]) -> ElementModP:
+        if isinstance(value, str):
+            elementmodp = hex_to_p(value)
+        else:
+            elementmodp = int_to_p(value)
+        if elementmodp is None:
             raise ValueError("invalid key: " + value)
-        return elementModP
+        return elementmodp
 
     @staticmethod
-    def string_to_elementModQ(value: str) -> ElementModQ:
-        elementModQ = hex_to_q(value)
-        if elementModQ is None:
+    def string_to_elementModQ(value: Union[int, str]) -> ElementModQ:
+        if isinstance(value, str):
+            elementmodq = hex_to_q(value)
+        else:
+            elementmodq = int_to_q(value)
+        if elementmodq is None:
             raise ValueError("invalid key: " + value)
-        return elementModQ
+        return elementmodq
