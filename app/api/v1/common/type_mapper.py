6b62e33b000cbdfa9f470b2b5e675857878428db
Lee Richardson <github@leerichardson.com>
2022-02-03 17:03:11 -0500

    Better error messages per PR

--- app/api/v1/common/type_mapper.py
+++ app/api/v1/common/type_mapper.py
@@ -13,26 +13,30 @@ class TypeMapper:
     def string_to_element_mod_p(value: str) -> ElementModP:
         elementmodp = hex_to_p(value)
         if elementmodp is None:
-            raise ValueError("invalid key: " + value)
+            raise ValueError(TypeMapper.type_error_message(value, "element_mod_q"))
         return elementmodp
 
     @staticmethod
     def string_to_element_mod_q(value: str) -> ElementModQ:
         elementmodq = hex_to_q(value)
         if elementmodq is None:
-            raise ValueError("invalid key: " + value)
+            raise ValueError(TypeMapper.type_error_message(value, "element_mod_q"))
         return elementmodq
 
     @staticmethod
     def int_to_element_mod_p(value: int) -> ElementModP:
         elementmodp = int_to_p(value)
         if elementmodp is None:
-            raise ValueError("invalid key: " + str(value))
+            raise ValueError(TypeMapper.type_error_message(str(value), "element_mod_q"))
         return elementmodp
 
     @staticmethod
     def int_to_element_mod_q(value: int) -> ElementModQ:
         elementmodq = int_to_q(value)
         if elementmodq is None:
-            raise ValueError("invalid key: " + str(value))
+            raise ValueError(TypeMapper.type_error_message(str(value), "element_mod_q"))
         return elementmodq
+
+    @staticmethod
+    def type_error_message(value: str, type: str):
+        return f"{value} cannot be converted to {type}."
