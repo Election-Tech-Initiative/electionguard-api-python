9585c7453041a2a9b854ab3b9a961ee8d246a4f3
Lee Richardson <github@leerichardson.com>
2022-02-03 17:15:38 -0500

    More PR Feedback
    Type mapper now accepts a Union[int, str]

--- app/api/v1/common/type_mapper.py
+++ app/api/v1/common/type_mapper.py
@@ -1,3 +1,4 @@
+from typing import Union
 from electionguard.group import (
     ElementModP,
     ElementModQ,
@@ -10,33 +11,19 @@ from electionguard.group import (
 
 class TypeMapper:
     @staticmethod
-    def string_to_element_mod_p(value: str) -> ElementModP:
-        elementmodp = hex_to_p(value)
-        if elementmodp is None:
-            raise ValueError(TypeMapper.type_error_message(value, "element_mod_q"))
-        return elementmodp
+    def string_to_element_mod_p(value: Union[int, str]) -> ElementModP:
+        element = int_to_p(value) if isinstance(value, int) else hex_to_p(value)
+        if element is None:
+            raise ValueError(TypeMapper.type_error_message(str(value), "element_mod_p"))
+        return element
 
     @staticmethod
-    def string_to_element_mod_q(value: str) -> ElementModQ:
-        elementmodq = hex_to_q(value)
-        if elementmodq is None:
-            raise ValueError(TypeMapper.type_error_message(value, "element_mod_q"))
-        return elementmodq
-
-    @staticmethod
-    def int_to_element_mod_p(value: int) -> ElementModP:
-        elementmodp = int_to_p(value)
-        if elementmodp is None:
+    def string_to_element_mod_q(value: Union[int, str]) -> ElementModQ:
+        element = int_to_q(value) if isinstance(value, int) else hex_to_q(value)
+        if element is None:
             raise ValueError(TypeMapper.type_error_message(str(value), "element_mod_q"))
-        return elementmodp
+        return element
 
     @staticmethod
-    def int_to_element_mod_q(value: int) -> ElementModQ:
-        elementmodq = int_to_q(value)
-        if elementmodq is None:
-            raise ValueError(TypeMapper.type_error_message(str(value), "element_mod_q"))
-        return elementmodq
-
-    @staticmethod
-    def type_error_message(value: str, type: str):
+    def type_error_message(value: str, type: str) -> str:
         return f"{value} cannot be converted to {type}."
