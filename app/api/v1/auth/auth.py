a3e6f8808187808837ee9276a872cbc95b1abd2d
Lee Richardson <github@leerichardson.com>
2022-02-08 13:18:26 -0500

    Fix bug with settings being injected into UserScope API's

--- app/api/v1/auth/auth.py
+++ app/api/v1/auth/auth.py
@@ -49,11 +49,10 @@ class ScopedTo(params.Depends):
     def __call__(
         self,
         request: Request,
-        settings: Settings = Settings(),
         token: str = Security(oauth2_scheme),
     ) -> TokenData:
         """Check scopes and return the current user."""
-        data = validate_access_token(settings, token)
+        data = validate_access_token(Settings(), token)
         validate_access_token_authorization(data, self._scopes)
         return data
 
