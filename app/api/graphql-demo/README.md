c203ca20d9d39f04530d93b336bf398b884d0fdd
Gagan Deep <gagandeeppratihar@outlook.com>
2022-10-04 12:50:07 +0530

    graphql support added

---
+++ app/api/graphql-demo/README.md
@@ -0,0 +1,1 @@
+https://strawberry.rocks/docs
---
+++ app/api/graphql-demo/schema.py
@@ -0,0 +1,22 @@
+import typing
+import strawberry
+
+def get_books():
+    return [
+        Book(
+            title='The Great Gatsby',
+            author='F. Scott Fitzgerald',
+        ),
+    ]
+
+@strawberry.type
+class Book:
+    title: str
+    author: str
+
+@strawberry.type
+class Query:
+    books: typing.List[Book] = strawberry.field(resolver=get_books)
+
+
+schema = strawberry.Schema(query=Query)
